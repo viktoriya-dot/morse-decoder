1fd0122e3b05e682686889a9bfc0dad2aa8e562e
viktoriya-dot <201990kg@gmail.com>
2022-08-11 12:40:16 +0600

    feat:add solution

--- src/index.js
+++ src/index.js
@@ -37,9 +37,11 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
-}
+
+decode = (string) => {
+    let arrMorse = string.match(/.{1,10}/g).map((item) => item.replace(/00/g,'').replace(/10/g,'.').replace(/11/g,'-'));
+    return (arrMorse.map(item => (item === '**********') ? ' ' : MORSE_TABLE[item])).join('');
+};
 
 module.exports = {
     decode
